48eb8366577777e8c94463c2a618236c9b8016d0
Binero <jbollensb@gmail.com>
2013-03-24 20:51:24 +0100

    Improved Security

--- config.php
+++ config.php
@@ -1,16 +1,2 @@
 <?php
-// phpBB 3.0.x auto-generated configuration file
-// Do not change anything in this file!
-$dbms = 'mysqli';
-$dbhost = '';
-$dbport = '';
-$dbname = 'board_board';
-$dbuser = 'board_board1';
-$dbpasswd = 'vergeten15';
-$table_prefix = 'alvt_';
-$acm_type = 'file';
-$load_extensions = '';
-
-@define('PHPBB_INSTALLED', true);
-// @define('DEBUG', true);
-// @define('DEBUG_EXTRA', true);
+require('./protected/config.php');
