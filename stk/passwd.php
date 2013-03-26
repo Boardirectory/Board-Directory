82fa88c589c56917c18de432915bf09ae24945d8
Binero <jbollensb@gmail.com>
2013-03-26 20:49:28 +0100

    Included Board Database + Included SDK + Reset Styles

---
+++ stk/passwd.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Support Toolkit emergency password.
+* The file was generated on: 26/Mar/2013 20:32.00 and will expire on: 27/Mar/2013 02:32.00.
+*/
+
+// This file can only be from inside the Support Toolkit
+if (!defined('IN_PHPBB') || !defined('STK_VERSION'))
+{
+	exit;
+}
+
+$stk_passwd				= 'Mj5AMaKJPeCRVRbIVaFbw8rAv';
+$stk_passwd_expiration	= 1364347920;
