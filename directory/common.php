942b8634b08ff50a960cf1ac975f95ab03892c9c
Binero <jbollensb@gmail.com>
2013-03-27 21:48:57 +0100

    Implemented Error Handler

--- directory/common.php
+++ directory/common.php
@@ -4,19 +4,19 @@ if(!defined('IN_DIRECTORY')) {
 }
 session_name('bis');
 $phpbb_root_path = './../';
+include_once($directory_root_path . 'classes/errorhandler.php');
+ErrorHandler::setErrorHandler();
 include_once($directory_root_path . 'includes/constants.php');
 include_once($directory_root_path . 'includes/commonfunctions.php');
 include_once($directory_root_path . 'classes/user.php');
 include_once($directory_root_path . 'classes/bis.php');
 include_once($directory_root_path . 'classes/template.php');
-try {
-	$db = new PDO('mysql:host=localhost;dbname=bis', 'bis', base64_decode(file_get_contents($directory_root_path . 'includes/dbpass')), array(
-		PDO::ATTR_PERSISTENT	=> true,
-	));
-	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-} catch(PDOException $ex) {
-	trigger_error($ex->getMessage(), E_USER_ERROR);
-}
+
+$db = new PDO('mysql:host=localhost;dbname=bis', 'bis', base64_decode(file_get_contents($directory_root_path . 'includes/dbpass')), array(
+	PDO::ATTR_PERSISTENT	=> true,
+));
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 $user = new User();
 $bis = new BIS();
 $template = new DirectoryTemplate();
